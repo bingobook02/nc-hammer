f5ed24cc8433695451a20824cefb3cc28c7389b0
Ahmed Keddar <akeddar@juniper.net>
2018-07-23 13:41:20 +0100

    refactoring code, action.go tests added

--- action/action_test.go
+++ action/action_test.go
@@ -12,44 +12,30 @@ import (
 )
 
 func Test_Execute(t *testing.T) {
-	ts1, _ := suite.NewTestSuite("../suite/testdata/test-suite.yml")           // testsuite with netconf actions
-	ts2, _ := suite.NewTestSuite("../suite/testdata/testsuite-sleep.yml")      // testsuite with sleep actions
-	ts3, _ := suite.NewTestSuite("../suite/testdata/testsuite-failblocks.yml") // test sleep with no sleep or netconf actions
 	var buff bytes.Buffer
 	var want string
-
-	assertCheck := func(ts *suite.TestSuite) {
-		t.Helper()
-		start := time.Now()
-		resultChannel := make(chan result.NetconfResult)
-		handleResultsFinished := make(chan bool)
-		go result.HandleResults(resultChannel, handleResultsFinished, ts)
-		block := ts.GetInitBlock()
-		for _, a := range block.Actions {
-			if ts == ts1 {
-				Execute(start, 0, ts, a, resultChannel)
-				assert.NotNil(t, a.Netconf)
-			} else if ts == ts2 {
-				Execute(start, 0, ts, a, resultChannel)
-				assert.NotNil(t, a.Sleep)
-			} else {
+	ts1, _ := suite.NewTestSuite("../suite/testdata/test-suite.yml")             // testsuite with netconf actions
+	ts2, _ := suite.NewTestSuite("../suite/testdata/testsuite-fail-actions.yml") // testsuite with no netconf or sleep actions
+	myTests := []*suite.TestSuite{ts1, ts2}
+	start := time.Now()
+	resultChannel := make(chan result.NetconfResult)
+	handleResultsFinished := make(chan bool)
+	go result.HandleResults(resultChannel, handleResultsFinished, ts1)
+	for _, myTs := range myTests {
+		for _, b := range myTs.Blocks {
+			for _, a := range b.Actions {
 				log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 				log.SetOutput(&buff)
-				Execute(start, 0, ts, a, resultChannel)
-				got := buff.String()
-				want += "\n ** Problem with your Testsuite, an action in a block section has incorrect YAML indentation for its body, ensure that anything after netconf or sleep is properly indented **\n\n"
-				assert.Equal(t, want, got)
+				if myTs == ts1 {
+					Execute(start, 0, ts1, a, resultChannel)
+					assert.True(t, (a.Sleep != nil) || (a.Netconf != nil)) // checks for netconf or sleep actions
+				} else {
+					Execute(start, 0, ts2, a, resultChannel)
+					got := buff.String()
+					want += "\n ** Problem with your Testsuite, an action in a block section has incorrect YAML indentation for its body, ensure that anything after netconf or sleep is properly indented **\n\n"
+					assert.Equal(t, want, got)
+				}
 			}
 		}
 	}
-	t.Run("test an action passed is netconf", func(t *testing.T) {
-		assertCheck(ts1)
-	})
-	t.Run("test an action passed is sleep", func(t *testing.T) {
-		assertCheck(ts2)
-	})
-	t.Run("test bad indent or nil netconf&sleep", func(t *testing.T) {
-		assertCheck(ts3)
-	})
-
 }
